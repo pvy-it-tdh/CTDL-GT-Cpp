bd91fdebde3df48f3d5305b993f5eb0bde489b21
pvy-it-tdh <phucvy107@gmail.com>
2024-03-31 22:56:57 +0700

    U

--- Sort/mergesort.cpp
+++ Sort/mergesort.cpp
@@ -1,58 +1,59 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-void merge(int a[],int l,int m,int r)
+void merge(int a[], int l, int m, int r)
 {
-    vector<int>x(a+l,a+m+1);
-    vector<int>y(a+m+1,a+r+1);
-    int i=0;
-    int j=0;
-    while(i<x.size()&&j<y.size())
+    vector<int> x(a + l, a + m + 1);
+    vector<int> y(a + m + 1, a + r + 1);
+    int i = 0;
+    int j = 0;
+    while (i < x.size() && j < y.size())
     {
-        if(x[i]<y[j])
+        if (x[i] < y[j])
         {
-            a[l]=x[i];
+            a[l] = x[i];
             ++i;
             ++l;
         }
-        else{
-            a[l]=y[j];
+        else
+        {
+            a[l] = y[j];
             ++j;
             ++l;
         }
     }
-    while (i<x.size())
+    while (i < x.size())
     {
-        a[l]=x[i];
+        a[l] = x[i];
         ++l;
         ++i;
-    } 
-    while (j<y.size())
+    }
+    while (j < y.size())
     {
-        a[l]=y[j];
+        a[l] = y[j];
         ++l;
         ++j;
     }
 }
-void mergeSort(int a[],int l, int r)
+void mergeSort(int a[], int l, int r)
 {
-    if(l>=r)
-    return;
-    int m=(l+r)/2;
-    mergeSort(a,l,m);
-    mergeSort(a,m+1,r);
-    merge(a,l,m,r);
+    if (l >= r)
+        return;
+    int m = (l + r) / 2;
+    mergeSort(a, l, m);
+    mergeSort(a, m + 1, r);
+    merge(a, l, m, r);
 }
 int main()
 {
     int n;
-    int *a= new int[n];
-    for (int i=0;i<n;i++)
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-    mergeSort(a,0,n-1);
-    for(int i=0;i<n;i++)
+    mergeSort(a, 0, n - 1);
+    for (int i = 0; i < n; i++)
     {
         cout << a[i] << ' ';
     }
