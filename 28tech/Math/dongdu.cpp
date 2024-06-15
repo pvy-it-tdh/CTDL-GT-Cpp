b99bce09fe884b39690108fd4edc8b23d8bd73f5
pvy-it-tdh <phucvy107@gmail.com>
2024-06-15 19:09:00 +0700

    add đồng dư

--- 28tech/Math/dongdu.cpp
+++ 28tech/Math/dongdu.cpp
@@ -2,4 +2,28 @@
 (A+B)%C= ((A%C)+(B%C))%C
 (A-B)%C= ((A%C)-(B%C)+C)%C
 (A*B)%C= ((A%C)*(B%C))%C
+(A^B)%C=(A%C)^B%C
+
+(A/B)%C=(A%C*B^-1)%C
 */
+
+#include <iostream>
+using ll = long long;
+ll MOD = 1000000007;
+// (a^b)%C
+ll powMOD(ll a, ll b, ll c)
+{
+    ll res = 1;
+    a = a % c;
+    for (int i = 1; i <= b; i++)
+    {
+        res = res * a;
+        res = res % c;
+    }
+    return res;
+}
+using namespace std;
+int main()
+{
+    cout << powMOD(7, 10, 3) << endl;
+}
